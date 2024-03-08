01e29288ff832c69962a61756ea61239d8d29e7f
alejandropm <geo.al3x@gmail.com>
2024-03-08 17:36:43 +0100

    Add OpenAI dependency and integrate AI query functionality

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,28 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+dotenv.config();
+
 // Open AI
 
+import OpenAI from "openai";
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+async function queryAI(input: string) {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `Answer to the next query in as few words as possible: ${input}`,
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+  console.log(completion.choices[0]);
+}
 
 // App
-
-dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -15,25 +31,15 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_, res: Response) => {
   res.send("Express + TypeScript Server");
-});
-
-const mockNoteData = {
-  title: "Hello World",
-  text: "This is a simple note",
-};
-
-app.get("/test", (req: Request, res: Response) => {
-  setTimeout(() => {
-    res.status(200).json({ ...mockNoteData });
-  }, 3000);
 });
 
 app.post("/ai-test", (req: Request, res: Response) => {
   const input = req.body;
-  console.log(input);
-  res.status(200).json("ok");
+  console.log("input", input);
+  const aiOutput = queryAI(input);
+  res.status(200).json(aiOutput);
 });
 
 app.listen(port, () => {
