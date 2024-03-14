f373b497dcbb5e7a43df82ca4f4c28c13a0795bd
alejandropm <geo.al3x@gmail.com>
2024-03-14 18:16:50 +0100

    Refactor queryAI to work with array embedded in an object

--- src/app.ts
+++ src/app.ts
@@ -29,8 +29,8 @@ async function queryAI(input: string, typeId: number) {
 
   const content =
     typeId === 0
-      ? `Provide a brief answer to the next query: ${input}. (25 words max)`
-      : `Respond to the next query with a concise JSON array: ${input}. (Up to 10 items, 10 words per item)`;
+      ? `Provide a brief answer to the next query: "${input}". (25 words max)`
+      : `Respond to the next query with a concise JSON array: "${input}". (Up to 10 items, 10 words per item)`;
 
   const completion = await openai.chat.completions.create({
     messages: [
@@ -43,11 +43,17 @@ async function queryAI(input: string, typeId: number) {
   });
 
   // console.log(">>>", completion);
-  // console.log(">>", completion.choices[0].message.content);
+  console.log(">>", completion.choices[0].message.content);
 
   if (typeId === 1 && completion.choices[0].message.content) {
-    const res = JSON.parse(completion.choices[0].message.content);
-    // console.log(">>>", res);
+    let res = JSON.parse(completion.choices[0].message.content);
+    // console.log("1>>>", res);
+
+    // If res is an object, convert it to an array
+    if (typeof res === "object" && !Array.isArray(res)) {
+      res = Object.values(res).flat();
+      // console.log("2>>>", res);
+    }
     const array = res.map((item: string) => ({
       itemId: crypto.randomUUID(),
       item,
